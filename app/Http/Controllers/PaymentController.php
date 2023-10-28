24e6b1796ee4aaa1175e3ac1ed0a406b176ab57a
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2023-10-27 18:11:04 -0700

    implemented stripe

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -90,7 +90,7 @@ class PaymentController extends Controller{
     }
 }*/
 
-namespace App\Http\Controllers;
+/*namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -164,7 +164,105 @@ class PaymentController extends Controller
     {
         // Handle cancellation
     }
+}*/
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Stripe\Exception\InvalidRequestException;  // Import the InvalidRequestException class
+use Stripe\Stripe;
+
+class PaymentController extends Controller
+{
+    public function payment(Request $request)
+    {
+        try {
+            // Set your secret key. Remember to switch to your live secret key in production.
+            // See your keys here: https://dashboard.stripe.com/apikeys
+            Stripe::setApiKey(config('stripe.stripe_sk'));
+
+            // Convert the amount to cents for LKR
+            $totalAmountCents = $request->price * 100;
+
+            // Ensure the total amount is at least 50 cents
+            $minimumAmountCents = 50;
+
+            // Log the total amount for debugging
+            Log::info("Total Amount (Cents): $totalAmountCents");
+
+            if ($totalAmountCents < $minimumAmountCents) {
+                // Log an error message for debugging
+                Log::error("Total amount is less than $minimumAmountCents cents.");
+
+                // Throw a custom exception for amounts less than 50 cents
+                throw new \Exception('The total amount must be at least 50 cents.');
+            }
+
+            $cart = session()->get('cart');
+
+            $totalPriceCents = 0;
+            foreach ($cart as $item) {
+                $totalPriceCents += $item['product']->price * $item['quantity'] * 100;
+            }
+
+            
+           
+
+            $line_items = [];
+            foreach ($cart as $item) {
+                $line_items[] = [
+                    'price_data' => [
+                        'currency' => 'lkr',
+                        'product_data' => ['name' => $item['product']->name],
+                        
+                        'unit_amount' => $item['product']->price * 100,
+                        
+                    ],
+                    'quantity' => $item['quantity'],
+                    
+                    
+                ];
+            }
+            
+
+            // Set the Stripe Checkout Session unit amount.
+            $response = \Stripe\checkout\Session::create([
+                'line_items' => $line_items,
+                'mode' => 'payment',
+                'allow_promotion_codes' => true,
+                'success_url' => route('stripe_success'),
+                'cancel_url' => route('stripe_cancel'),
+                
+            ]);
+
+            echo '<p>Total due: LKR ' . $totalPriceCents / 100 . '</p>';
+
+            
+
+            return redirect()->away($response->url);
+        } catch (InvalidRequestException $e) {
+            // Catch Stripe-specific exceptions
+            Log::error("Stripe Error: " . $e->getMessage());
+            return redirect()->route('cart')->with('error', 'There was an error processing your payment.');
+        } catch (\Exception $e) {
+            // Catch other exceptions
+            Log::error("Error: " . $e->getMessage());
+            return redirect()->route('cart')->with('error', $e->getMessage());
+        }
+    }
+
+    public function success()
+    {
+        // Handle success
+    }
+
+    public function cancel()
+    {
+        // Handle cancellation
+    }
 }
+
 
 
     

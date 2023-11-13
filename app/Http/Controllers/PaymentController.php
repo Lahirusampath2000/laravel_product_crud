f8dd1780879d78a5566913105138828ce72fcec8
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2023-11-13 10:24:28 -0800

    invoice

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -1,177 +1,18 @@
 <?php
 
-/*namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-use Session;
-
-use Stripe;
-
-class PaymentController extends Controller
-{
-    public function stripe()
-    {
-        return view('stripe');
-    }
-
-
-
-    public function stripePost(Request $request)
-    {
-
-        Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
-        Stripe\Charge::create ([
-                "amount" => 100 * 100,
-                "currency" => "LKR",
-                "source" => $request->stripeToken,
-                "description" => "This payment is tested purpose"
-        ]);
-   
-        Session::flash('success', 'Payment successful!');
-           
-        return back();
-        
-    }
-}*/
-
-/*namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-use App\Model\product;
-
-
-class PaymentController extends Controller{
-
-    public function payment(Request $request){
-        // Set your secret key. Remember to switch to your live secret key in production.
-        // See your keys here: https://dashboard.stripe.com/apikeys
-        \Stripe\Stripe::setApiKey(config('stripe.stripe_sk'));
-
-        $totalAmount = $request->price * 100;
-
-       
-        
-
-    // Set the Stripe Checkout Session unit amount.
-        
-
-        $response=\Stripe\checkout\Session::create([
-        'line_items' => [
-            [
-            'price_data' => [
-                'currency' => 'lkr',
-                'product_data' => ['name' => 'Total'],
-                //'unit_amount' =>$request->price*100,
-                'unit_amount' =>$totalAmount,
-                
-                
-            ],
-            
-            'quantity' => 1,
-            ],
-        ],
-        
-        'mode' => 'payment',
-        'success_url' => route('stripe_success'),
-        'cancel_url' => route('stripe_cancel'),
-        ]);
-
-        return redirect()->away($response->url);
-    }
-
-    public function success(){
-
-    }
-
-    public function cancel(){
-
-    }
-}*/
-
-/*namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
-use Stripe\Exception\InvalidRequestException;  // Import the InvalidRequestException class
-use Stripe\Stripe;
-
-class PaymentController extends Controller
-{
-    public function payment(Request $request)
-    {
-        try {
-            // Set your secret key. Remember to switch to your live secret key in production.
-            // See your keys here: https://dashboard.stripe.com/apikeys
-            Stripe::setApiKey(config('stripe.stripe_sk'));
-
-            // Convert the amount to cents for LKR
-            $totalAmountCents = $request->price * 100;
-
-            // Ensure the total amount is at least 50 cents
-            $minimumAmountCents = 50;
-
-            // Log the total amount for debugging
-            Log::info("Total Amount (Cents): $totalAmountCents");
-
-            if ($totalAmountCents < $minimumAmountCents) {
-                // Log an error message for debugging
-                Log::error("Total amount is less than $minimumAmountCents cents.");
-
-                // Throw a custom exception for amounts less than 50 cents
-                throw new \Exception('The total amount must be at least 50 cents.');
-            }
-
-            // Set the Stripe Checkout Session unit amount.
-            $response = \Stripe\checkout\Session::create([
-                'line_items' => [
-                    [
-                        'price_data' => [
-                            'currency' => 'lkr',
-                            'product_data' => ['name' => 'Total'],
-                            'unit_amount' => $totalAmountCents,
-                        ],
-                        'quantity' => 1,
-                    ],
-                ],
-                'mode' => 'payment',
-                'success_url' => route('stripe_success'),
-                'cancel_url' => route('stripe_cancel'),
-                
-            ]);
-
-            
-
-            return redirect()->away($response->url);
-        } catch (InvalidRequestException $e) {
-            // Catch Stripe-specific exceptions
-            Log::error("Stripe Error: " . $e->getMessage());
-            return redirect()->route('cart')->with('error', 'There was an error processing your payment.');
-        } catch (\Exception $e) {
-            // Catch other exceptions
-            Log::error("Error: " . $e->getMessage());
-            return redirect()->route('cart')->with('error', $e->getMessage());
-        }
-    }
-
-    public function success()
-    {
-        // Handle success
-    }
-
-    public function cancel()
-    {
-        // Handle cancellation
-    }
-}*/
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Stripe\Exception\InvalidRequestException;  // Import the InvalidRequestException class
 use Stripe\Stripe;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ReceiptEmail;
+
+use Illuminate\Support\Facades\View;
 
 class PaymentController extends Controller
 {
@@ -231,7 +72,7 @@ class PaymentController extends Controller
                 'line_items' => $line_items,
                 'mode' => 'payment',
                 'allow_promotion_codes' => true,
-                'success_url' => route('stripe_success'),
+                'success_url' => route('stripe_success') . '?session_id={CHECKOUT_SESSION_ID}',
                 'cancel_url' => route('stripe_cancel'),
                 
             ]);
@@ -252,16 +93,117 @@ class PaymentController extends Controller
         }
     }
 
-    public function success()
+    public function success(Request $request)
     {
-        // Handle success
-    }
+        /*Log::info('Entering success method.');
+
+        try {
+            // Set your secret key. Make sure to replace 'your_stripe_secret_key_here' with your actual secret key.
+            Stripe::setApiKey(config('stripe.stripe_sk'));
+
+            // Retrieve the session ID from the request
+            $sessionId = $request->query('session_id');
+
+            // Retrieve the session to get details about the payment
+            $session = \Stripe\Checkout\Session::retrieve($sessionId);
+
+            // Add logic to determine payment status based on the session or other criteria
+            $paymentStatus = 'Payment Successful'; // Adjust this based on your actual logic
+
+            $cart = session('cart');
+
+            // Retrieve the cart and other data as needed
+            $cart = []; // Replace this with the actual logic to retrieve the cart data
+
+            Log::info('Success block executed.');
+            
+
+            return view('success', [
+                'paymentStatus' => $paymentStatus,
+                'cart' => $cart,
+                'session' => $session,
+                //'lineItems' => $lineItems,
+            ]);
+        } catch (InvalidRequestException $e) {
+            // Catch Stripe-specific exceptions
+            Log::error("Stripe Error: " . $e->getMessage());
+            return redirect()->route('cart')->with('error', 'There was an error processing your payment.');
+        } catch (\Exception $e) {
+            // Catch other exceptions
+            Log::error("Error in success method: " . $e->getMessage());
+            return redirect()->route('cart')->with('error', 'There was an error processing your payment.');
+        }*/
+
+        Log::info('Entering success method.');
+
+        try {
+            // Set your secret key. Make sure to replace 'your_stripe_secret_key_here' with your actual secret key.
+            Stripe::setApiKey(config('stripe.stripe_sk'));
+    
+            // Retrieve the session ID from the request
+            $sessionId = $request->query('session_id');
+    
+            // Retrieve the session to get details about the payment
+            $session = \Stripe\Checkout\Session::retrieve($sessionId);
+    
+            // Add logic to determine payment status based on the session or other criteria
+            $paymentStatus = 'Payment Successful'; // Adjust this based on your actual logic
+    
+            
+            
+    
+            // Retrieve line items from the session
+            $cart = session()->get('cart', []);
+
+            $line_items = $session->display_items;
+
+            /*$emailContent = view('emails.receipt', [
+                'paymentStatus' => $paymentStatus,
+                'cart' => $cart,
+                'session' => $session,
+            ])->render();
+
+            $customerEmail='phlsampath2000@gmail.com';
+
+            Mail::to($customerEmail)->send(new ReceiptEmail($paymentStatus, $cart, $session));*/
+    
+            Log::info('Success block executed.');
+
+            
+    
+            return view('success', [
+                'paymentStatus' => $paymentStatus,
+                'cart' => $cart,
+                'session' => $session,
+                'lineItems' => $line_items,
+                // Add other data as needed
+            ]);
+            
+        } catch (InvalidRequestException $e) {
+            // Catch Stripe-specific exceptions
+            Log::error("Stripe Error: " . $e->getMessage());
+            return redirect()->route('cart')->with('error', 'There was an error processing your payment.');
+        } catch (\Exception $e) {
+            // Catch other exceptions
+            Log::error("Error in success method: " . $e->getMessage());
+            return redirect()->route('cart')->with('error', 'There was an error processing your payment.');
+        }
+
+}    
+    
+
+    
+
+    
+    
 
     public function cancel()
     {
         // Handle cancellation
     }
 }
+
+
 
 
 
